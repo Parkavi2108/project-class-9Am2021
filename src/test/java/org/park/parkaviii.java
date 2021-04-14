7459dbb8730ce60e0c8384cd2a07fec3cb436edf
parkavi2108 <prkv2108@gmail.com>
2021-04-14 12:28:01 +0530

    Class Generated

---
+++ src/test/java/org/park/parkaviii.java
@@ -0,0 +1,13 @@
+package org.park;
+
+public class parkaviii {
+		private void bank() {
+		System.out.println("savings");
+		}
+		public static void main(String[] args) {
+			parkaviii a=new parkaviii();
+			a.bank();
+		}
+		}
+
+
